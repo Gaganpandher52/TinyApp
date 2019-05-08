0b7816fb8d66e1ed628b49875e8e58a1b444a1f8
Gaganpandher52 <gaganpandher52@gmail.com>
2019-05-08 01:32:19 +0000

    working with post

--- express.server.js
+++ express.server.js
@@ -2,12 +2,22 @@
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs")
-
+const generateRandomString = () => Math.random().toString(36).substring(2, 8);
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  });
+
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -27,6 +37,11 @@ app.get("/urls/:shortURL", (req, res) => {
     let templateVars = { shortURL: shortU, longURL: urlDatabase[shortU] };
     res.render("urls_show", templateVars);
   });
+
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
